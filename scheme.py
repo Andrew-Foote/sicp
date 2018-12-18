b88fb7a293a4443daf7d53d6af42dac541c33687
Andrew-Foote <andrewfoote@btinternet.com>
2018-12-18 22:58:07 +0000

    add a lexer

--- scheme.py
+++ scheme.py
@@ -1,5 +1,27 @@
+import string
 import sys
 import typing as t
+
+def lex(s: str) -> t.Iterator[str]:
+    """Lex a string of Scheme source code, returning an iterator over
+    its tokens."""
+    token: t.List[str] = []
+
+    def flush() -> t.Iterator[str]:
+        if token:
+            yield ''.join(token)
+            token.clear()
+
+    for c in s:
+        if c in string.whitespace:
+            yield from flush()
+        elif c in ('(', ')'):
+            yield from flush()
+            yield c
+        else:
+            token.append(c)
+
+    yield from flush()
 
 if __name__ == '__main__':
     while True:
@@ -9,4 +31,4 @@ if __name__ == '__main__':
         if s == 'exit':
             sys.exit()
         
-        print(s)
+        print(' '.join(lex(s)))
